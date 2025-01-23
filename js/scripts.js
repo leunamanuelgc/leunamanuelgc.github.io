8c568f420fbcc8e6b4455639899095abacd23908
m4nu3lgc <loloandthebeastcactus@gmail.com>
2025-01-24 00:53:18 +0100

    Finikitao

--- js/scripts.js
+++ js/scripts.js
@@ -78,3 +78,28 @@ function fadeIn(el, display) {
         }
     })();
 };
+
+// Copy To Clipboard
+document.querySelectorAll(".copy").forEach(copyButton =>{
+    copyButton.addEventListener("click",() =>{
+        const targetElement = document.querySelector(copyButton.dataset.copy);
+        const textToCopy = targetElement.textContent
+            .replace(/\s+/g, " ")
+            .trim();
+        console.log(targetElement);
+
+        navigator.clipboard.writeText(textToCopy).then(() => {
+            const label = copyButton.querySelector(".copy-label");
+            const originalText = label.textContent;
+
+            copyButton.disabled = true;
+            label.textContent = "Copied!";
+            
+            setTimeout(()=>{
+                copyButton.disabled = false;
+                label.textContent = originalText;
+            }, 1000);
+        });
+        
+    })
+})
